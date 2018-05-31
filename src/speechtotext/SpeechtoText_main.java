b201f7a6bc1dc1be85ebc870e5ed2574e596f699
j16005 <j16005@sangi.jp>
2018-05-31 12:19:36 +0900

    産業技術専門学校2018

---
+++ src/speechtotext/SpeechtoText_main.java
@@ -0,0 +1,51 @@
+package speechtotext;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechRecognitionResults;
+
+public class SpeechtoText_main {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		 SpeechToText service = new SpeechToText();
+		 service.setUsernameAndPassword("7bf2a504-26e0-4e66-93a8-d7b417432ac1", "aaVxbPjYkkbT");
+
+		    File audio = new File("./audio/output.wav");
+		    RecognizeOptions options =null;
+			try {
+				options = new RecognizeOptions.Builder()
+						.model("ja-JP_BroadbandModel")
+				    .audio(audio)
+				    .contentType(RecognizeOptions.ContentType.AUDIO_WAV)
+				    .build();
+			} catch (FileNotFoundException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		    SpeechRecognitionResults transcript = service.recognize(options).execute();
+
+		    JsonNode node=null;
+		    ObjectMapper mapper = new ObjectMapper();
+		    try {
+				 node = mapper.readTree(transcript.toString());
+			} catch (IOException e) {
+				// TODO 自動生成された catch ブロック
+				e.printStackTrace();
+			}
+		    System.out.println(transcript);
+		    String transcript1= node.get("results").get(0).get("alternatives").get(0).get("transcript").asText();
+		    System.out.println("transcript1 :"+transcript1);
+		    Double confidence = node.get("results").get(0).get("alternatives").get(0).get("confidence").asDouble();
+		    System.out.println("confidence :"+confidence);
+
+		}
+
+	}
+
