597d9c43c4476655b37009335129a1587f857850
j16005 <j16005@sangi.jp>
2018-06-07 16:35:12 +0900

    sangi2018

--- src/speechtotext/SpeechtoText_main.java
+++ src/speechtotext/SpeechtoText_main.java
@@ -15,10 +15,12 @@ public class SpeechtoText_main {
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 		 SpeechToText service = new SpeechToText();
-		 service.setUsernameAndPassword("j16005", "j16005");
+		 service.setUsernameAndPassword("7bf2a504-26e0-4e66-93a8-d7b417432ac1", "aaVxbPjYkkbT");
 
 		    File audio = new File("./audio/output.wav");
 		    RecognizeOptions options =null;
+
+		    MySQL mysql = new MySQL();
 			try {
 				options = new RecognizeOptions.Builder()
 						.model("ja-JP_BroadbandModel")
@@ -31,19 +33,24 @@ public class SpeechtoText_main {
 			}
 		    SpeechRecognitionResults transcript = service.recognize(options).execute();
 
-		    JsonNode node=null;
+		    //JsonNode node=null;
+		    String s=String.valueOf(transcript);
 		    ObjectMapper mapper = new ObjectMapper();
 		    try {
-				 node = mapper.readTree(transcript.toString());
+				 //node = mapper.readTree(transcript.toString());
+		    	JsonNode node=mapper.readTree(s);
+		    	for(int i=0;i<node.get("results").size();i++){
+		    		String text = node.get("results").get(0).get("alternatives").get(0).get("transcript").toString();
+		    		System.out.println("transcript : "+text);
+		    		double confidence =node.get("results").get(0).get("alternatives").get(0).get("confidence").asDouble();
+		    		System.out.println("confidence : "+confidence);
+		    		mysql.updateImage(text,confidence);
+		    	}
 			} catch (IOException e) {
 				// TODO 自動生成された catch ブロック
 				e.printStackTrace();
 			}
-		    System.out.println(transcript);
-		    String transcript1= node.get("results").get(0).get("alternatives").get(0).get("transcript").asText();
-		    System.out.println("transcript1 :"+transcript1);
-		    Double confidence = node.get("results").get(0).get("alternatives").get(0).get("confidence").asDouble();
-		    System.out.println("confidence :"+confidence);
+
 
 		}
 
